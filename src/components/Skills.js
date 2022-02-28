75dd0f475ca4289820e624b191c260fdbb0d9c5f
Justin Miles <jdmiles90@gmail.com>
2022-02-28 14:54:51 -0600

    Add title and alt tags to Toolbox icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Accordion, Row, Col, Container } from 'react-bootstrap';
-import ProductivityCarousel from './ProductivityCarousel';
 
 export default function Toolbox() {
   return (
@@ -25,25 +24,44 @@ export default function Toolbox() {
               <img
                 class="techstack-logo px-3"
                 src="../../javascript.svg"
-                alt=""
+                title="JavaScript"
+                alt="JavaScript"
               />
               <img
                 class="techstack-logo px-3 mb-3"
                 src="../../react.svg"
-                alt=""
+                title="React"
+                alt="React"
               />
-              <img class="techstack-logo px-3" src="../../redux.svg" alt="" />
-              <img class="techstack-logo px-3" src="../../html-5.svg" alt="" />
-              <img class="techstack-logo px-3" src="../../css-3.svg" alt="" />
+              <img
+                class="techstack-logo px-3"
+                src="../../redux.svg"
+                title="Redux"
+                alt="Redux"
+              />
+              <img
+                class="techstack-logo px-3"
+                src="../../html-5.svg"
+                title="HTML"
+                alt="HTML"
+              />
+              <img
+                class="techstack-logo px-3"
+                src="../../css-3.svg"
+                title="CSS"
+                alt="CSS"
+              />
               <img
                 class="techstack-logo px-3"
                 src="../../bootstrap.svg"
-                alt=""
+                title="Bootstrap"
+                alt="Bootstrap"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../material-ui.svg"
-                alt=""
+                title="Material UI"
+                alt="Material UI"
               />
             </Accordion.Body>
           </Accordion.Item>
@@ -53,22 +71,26 @@ export default function Toolbox() {
               <img
                 class="techstack-logo px-3"
                 src="../../nodejs.svg"
-                alt="nodejs"
+                title="NodeJS"
+                alt="NodeJS"
               />
               <img
                 class="techstack-logo px-3 mb-3"
                 src="../../mongodb.svg"
-                alt="mongodb"
+                title="MongoDB"
+                alt="MongoDB"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../express.svg"
-                alt="express"
+                title="Express"
+                alt="Express"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../python.svg"
-                alt="python"
+                title="Python"
+                alt="Python"
               />
             </Accordion.Body>
           </Accordion.Item>
@@ -78,42 +100,50 @@ export default function Toolbox() {
               <img
                 class="techstack-logo px-3"
                 src="../../git-icon.svg"
-                alt="git"
+                title="Git"
+                alt="Git"
               />
               <img
                 class="techstack-logo px-3 mb-3"
                 src="../../github-icon.svg"
-                alt="github"
+                title="GitHub"
+                alt="GitHub"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../bash-icon.svg"
-                alt="bash"
+                title="Bash"
+                alt="Bash"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../powershell.svg"
-                alt="powershell"
+                title="PowerShell"
+                alt="PowerShell"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../postman-icon.svg"
-                alt="postman"
+                title="Postman"
+                alt="Postman"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../npm-icon.svg"
-                alt="npm"
+                title="NPM"
+                alt="NPM"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../yarn.svg"
-                alt="yarn"
+                title="Yarn"
+                alt="Yarn"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../google-analytics.svg"
-                alt="googleanalytics"
+                title="Google Analytics"
+                alt="Google Analytics"
               />
             </Accordion.Body>
           </Accordion.Item>
@@ -123,17 +153,20 @@ export default function Toolbox() {
               <img
                 class="techstack-logo px-3"
                 src="../../visual-studio-code.svg"
-                alt="vscode"
+                title="Visual Studio Code"
+                alt="Visual Studio Code"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../office-icon.svg"
-                alt="office"
+                title="Microsoft Office"
+                alt="Microsoft Office"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../dropbox.svg"
-                alt="dropbox"
+                title="Dropbox"
+                alt="Dropbox"
               />
             </Accordion.Body>
           </Accordion.Item>
@@ -143,61 +176,15 @@ export default function Toolbox() {
               <img
                 class="techstack-logo px-3"
                 src="../../slack-icon.svg"
-                alt="slack"
+                title="Slack"
+                alt="Slack"
               />
               <img
                 class="techstack-logo px-3"
                 src="../../discord-icon.svg"
-                alt="discord"
+                title="Discord"
+                alt="Discord"
               />
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="5">
-            <Accordion.Header>Test2</Accordion.Header>
-            <Accordion.Body>
-              <div class="slider">
-                <div class="slide-track">
-                  <div class="slide">
-                    <img
-                      class="techstack-logo"
-                      src="../../javascript.svg"
-                      alt=""
-                    />
-                  </div>
-                  <div class="slide">
-                    <img class="techstack-logo" src="../../react.svg" alt="" />
-                  </div>
-                  <div class="slide">
-                    <img class="techstack-logo" src="../../redux.svg" alt="" />
-                  </div>
-                  <div class="slide">
-                    <img class="techstack-logo" src="../../html-5.svg" alt="" />
-                  </div>
-                  <div class="slide">
-                    <img class="techstack-logo" src="../../css-3.svg" alt="" />
-                  </div>
-                  <div class="slide">
-                    <img
-                      class="techstack-logo"
-                      src="../../bootstrap.svg"
-                      alt=""
-                    />
-                  </div>
-                  <div class="slide">
-                    <img
-                      class="techstack-logo"
-                      src="../../material-ui.svg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-              </div>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="6">
-            <Accordion.Header>Test123</Accordion.Header>
-            <Accordion.Body>
-              <ProductivityCarousel />
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
