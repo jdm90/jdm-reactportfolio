ef3c62556c27d17eb5447261e4b235bd1894a06c
Justin Miles <jdmiles90@gmail.com>
2022-03-02 11:01:35 -0600

    Hide collapsible navbar on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function MainNav() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div>
-      <Navbar expand="lg">
+      <Navbar expand="lg" expanded={expanded}>
         <Container>
           <Navbar.Brand href="#home">Justin Miles</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            onClick={() => setExpanded(expanded ? false : 'expanded')}
+            aria-controls="basic-navbar-nav"
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
+              <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/toolbox">
+              <Nav.Link
+                as={Link}
+                to="/toolbox"
+                onClick={() => setExpanded(false)}
+              >
                 Toolbox
               </Nav.Link>
-              <Nav.Link as={Link} to="/experiences">
+              <Nav.Link
+                as={Link}
+                to="/experiences"
+                onClick={() => setExpanded(false)}
+              >
                 Experiences
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact">
+              <Nav.Link
+                as={Link}
+                to="/contact"
+                onClick={() => setExpanded(false)}
+              >
                 Contact
               </Nav.Link>
             </Nav>
